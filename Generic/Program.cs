85e8358f50a216c8147960354e338ee1f8f2cfec
Denis <reykh_denis@mail.ru>
2023-06-09 22:00:48 +0300

    +

---
+++ Generic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static T Sum<T>(T a, T b)
+    {
+        if (typeof(T) == typeof(string))
+        {
+            string result = a.ToString() + b.ToString();
+            return (T)(object)result;
+        }
+
+        throw new ArgumentException("Тип данных не поддерживается.");
+    }
+
+    public static void Main()
+    {
+        DateTime start = DateTime.Now;
+
+        string result = Sum("Hello", " World");
+
+        DateTime end = DateTime.Now;
+
+        TimeSpan time = end - start;
+        Console.WriteLine("Результат: " + result);
+        Console.WriteLine("Время выполнения: " + time.TotalMilliseconds + "мс");
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
+
+
---
+++ slowdynamic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static dynamic Sum(dynamic a, dynamic b)
+    {
+        return a + b;
+    }
+
+    public static void Main()
+    {
+        DateTime start = DateTime.Now;
+
+        dynamic result = Sum("Hello", " World");
+
+        DateTime end = DateTime.Now;
+
+        TimeSpan time = end - start;
+        Console.WriteLine("Результат: " + result);
+        Console.WriteLine("Время выполнения: " + time.TotalMilliseconds + "мс");
+        Console.ReadKey();
+    }
+}
